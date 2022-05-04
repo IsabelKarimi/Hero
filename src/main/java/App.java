356feb137229f2d412b085e90e1ac69a6652eb2c
Isabel Karimi <100405131+IsabelKarimi@users.noreply.github.com>
2022-05-04 23:48:58 +0300

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,4 @@
-import spark.ModelAndView;
-import spark.template.handlebars.HandlebarsTemplateEngine;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 import static spark.Spark.*;
 public class App {
